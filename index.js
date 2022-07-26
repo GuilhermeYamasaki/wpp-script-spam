d662767b88b18776b2060e2d3cffb215ca70587f
Guilherme Yuji Yamasaki <103781859+GuilhermeYamasaki@users.noreply.github.com>
2022-07-26 10:10:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-async function enviarScript(scriptText){
-    const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line),
-	main = document.querySelector("#main"),
-	textarea = main.querySelector(`div[contenteditable="true"]`)
+async function floodarChat(scriptText){
+    const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line)
+	const main = document.querySelector("#main")
+	const textarea = main.querySelector(`div[contenteditable="true"]`)
 
-	if(!textarea) throw new Error("Não há uma conversa aberta")
+	if(!textarea) {
+		throw new Error("Não há uma conversa aberta")
+	}
 
 	for(const line of lines){
-		console.log(line)
 
 		textarea.textContent = line
 		textarea.dispatchEvent(new InputEvent("input", { bubbles: true }));
 
 		(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click()
 		
-		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 125))
+		if(lines.indexOf(line) !== lines.length - 1) {
+			await new Promise(resolve => setTimeout(resolve, 125))
+		}
 	}
 
 	return lines.length
 }
 
-let breakline = "\n"
-let text = "hello" + breakline
-let repeat = text.repeat(100)
-enviarScript(repeat)
+
+const repeat = "hello\n".repeat(100)
+floodarChat(repeat)
